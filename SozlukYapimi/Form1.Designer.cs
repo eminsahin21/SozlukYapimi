e332a85f133d17318cdbf323bfa238b65bc6a62c
Mehmet Emin <mehmeteminsahin34@gmail.com>
2022-06-05 18:15:02 +0300

    Add project files.

---
+++ SozlukYapimi/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace SozlukYapimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Türkçe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İngilizce";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 24);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 24);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "GÜNCELLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(274, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SİL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(417, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(265, 184);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 272);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ SozlukYapimi/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace SozlukYapimi
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OleDb.12.0;Data Source=" + Application.StartupPath + "\\vt_sozluk.accdb");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                OleDbCommand eklekomutu = new OleDbCommand("insert into ingturkce values('"+textBox1.Text+"','"+textBox2.Text+"')", baglanti);
+                eklekomutu.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Sözcük veri tabanına eklendi...","Veri tabani işlemleri");
+                textBox1.Clear();
+                textBox2.Clear();
+                
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message+"Veri tabanı işlemleri");
+                baglanti.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                OleDbCommand guncelle_komutu = new OleDbCommand("update ingturkce set turkce='"+textBox2.Text+"' where ingilizce='"+textBox1.Text+"'", baglanti);
+                guncelle_komutu.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Sözcük veri tabnında güncellendi...", "Veri tabani işlemleri");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message + "Veri tabanı işlemleri");
+                baglanti.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                baglanti.Open();
+                OleDbCommand silme_komutu = new OleDbCommand("delete from ingturkce where ingilizce='"+textBox1.Text+"'", baglanti);
+                silme_komutu.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Sözcük veri tabanından silindi...", "Veri tabani işlemleri");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message + "Veri tabanı işlemleri");
+                baglanti.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                baglanti.Open();
+                OleDbCommand aramakomutu = new OleDbCommand("select * from ingturkce where ingilizce like '"+textBox1.Text+"%'", baglanti);
+                OleDbDataReader oku = aramakomutu.ExecuteReader();
+                while (oku.Read())
+                {
+                    listBox1.Items.Add(oku["ingilizce"].ToString() + "=" + oku["turkce"].ToString());                 
+                }
+                baglanti.Close();
+            }
+            catch (Exception aciklama)
+            {
+
+                MessageBox.Show(aciklama.Message + "Veri tabanı işlemleri");
+                baglanti.Close();
+            }
+        }
+    }
+}
